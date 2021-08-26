f55d74a762f1bedaf6c159d349e711c438663922
Lucas Batista <lucasdanbatista@hotmail.com>
2021-08-26 11:22:50 -0300

    Animates navigation

--- app/src/main/java/me/lucasbatista/pokedex/ui/controller/MainActivity.kt
+++ app/src/main/java/me/lucasbatista/pokedex/ui/controller/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material.ExperimentalMaterialApi
 import dagger.hilt.android.AndroidEntryPoint
 import me.lucasbatista.pokedex.ui.view.MainView
 import me.lucasbatista.pokedex.ui.view_model.PokemonListViewModel
 
+@ExperimentalAnimationApi
 @ExperimentalMaterialApi
 @ExperimentalFoundationApi
 @AndroidEntryPoint
--- app/src/main/java/me/lucasbatista/pokedex/ui/view/MainView.kt
+++ app/src/main/java/me/lucasbatista/pokedex/ui/view/MainView.kt
@@ -1,23 +1,25 @@
 package me.lucasbatista.pokedex.ui.view
 
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.Composable
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import coil.annotation.ExperimentalCoilApi
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.composable
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import me.lucasbatista.pokedex.persistence.Pokemon
 
+@ExperimentalAnimationApi
 @ExperimentalCoilApi
 @ExperimentalMaterialApi
 @ExperimentalFoundationApi
 @Composable
 fun MainView(pokemons: List<Pokemon>) {
     val uiController = rememberSystemUiController()
-    val navController = rememberNavController()
-    NavHost(navController, "/pokemons") {
+    val navController = rememberAnimatedNavController()
+    AnimatedNavHost(navController, "/pokemons") {
         composable("/pokemons") {
             PokemonListView(pokemons, navController, uiController)
         }
